3da0c89810fc755c93a6f031a1296242bca593be
taka7mz <m.taka5prog5@gmail.com>
2021-09-26 14:57:17 +0900

    バリデーション追加

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,11 +11,13 @@
             @csrf
             <div class="title">
                 <h2>タイトル</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" />
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>本文</h2>
-                <textarea name="post[body]" placeholder="ここに入力"></textarea>
+                <textarea name="post[body]" placeholder="ここに入力"> {{ old('post.body') }}</textarea>
+                <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="新規登録" />
         </form>
