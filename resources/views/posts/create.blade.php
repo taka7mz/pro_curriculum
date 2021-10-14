9accc047970540083b9e40768cd12dc093ced113
taka7mz <m.taka5prog5@gmail.com>
2021-10-15 00:51:10 +0900

    ログイン機能追加

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,28 +1,32 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-    </head>
-    
-    <body>
-        <h1>Blog Site</h1>
-        <form action="/posts" method="post">
-            @csrf
-            <div class="title">
-                <h2>タイトル</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
-                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+@extends('layouts.app')
+
+@section('content')
+    <!DOCTYPE html>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+        <head>
+            <meta charset="utf-8">
+            <title>Blog</title>
+        </head>
+        
+        <body>
+            <h1>Blog Site</h1>
+            <form action="/posts" method="post">
+                @csrf
+                <div class="title">
+                    <h2>タイトル</h2>
+                    <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                    <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+                </div>
+                <div class="body">
+                    <h2>本文</h2>
+                    <textarea name="post[body]" placeholder="ここに入力"> {{ old('post.body') }}</textarea>
+                    <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
+                </div>
+                <input type="submit" value="新規登録" />
+            </form>
+            <div class = 'footer'>
+                <a href='/'>back</a>
             </div>
-            <div class="body">
-                <h2>本文</h2>
-                <textarea name="post[body]" placeholder="ここに入力"> {{ old('post.body') }}</textarea>
-                <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
-            </div>
-            <input type="submit" value="新規登録" />
-        </form>
-        <div class = 'footer'>
-            <a href='/'>back</a>
-        </div>
-    </body>
-</html>
+        </body>
+    </html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware'=>'auth'], function(){
+    Route::get('/', 'PostController@index')->middleware('auth');
+    Route::get('/posts/create', 'PostController@create');
+    Route::get('/posts/{post}', 'PostController@show');
+    Route::get('/posts/{post}/edit', 'PostController@edit');
+    Route::delete('/posts/{post}', 'PostController@delete');
+    Route::put('/posts/{post}', 'PostController@update');
+    Route::post('/posts', 'PostController@store');
+});
 
-Route::get('/', 'PostController@index');
-Route::get('/posts/create', 'PostController@create');
-Route::get('/posts/{post}', 'PostController@show');
-Route::get('/posts/{post}/edit', 'PostController@edit');
-Route::delete('/posts/{post}', 'PostController@delete');
-Route::put('/posts/{post}', 'PostController@update');
-Route::post('/posts', 'PostController@store');
 
 
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
